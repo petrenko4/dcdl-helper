ad16185bfc4916fb357a967daaed231f78e458f4
petrenko4 <akinol841@gmail.com>
2025-04-28 16:39:51 +0200

    initial commit

---
+++ build/debug/CMakeFiles/dcdl-helper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dcdl-helper.dir/src/main.cpp.o"
+  "CMakeFiles/dcdl-helper.dir/src/main.cpp.o.d"
+  "bin/dcdl-helper"
+  "bin/dcdl-helper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dcdl-helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
